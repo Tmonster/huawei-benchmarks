2f9e5643759ce9be633a6476cf7c4f77b0d39c37
Tom Ebergen <tom@ebergen.com>
2023-11-29 13:37:27 +0100

    update format_and_mount.sh

--- utils/format_and_mount.sh
+++ utils/format_and_mount.sh
@@ -1,18 +1,18 @@
 # script to format mount and copy data.
 
 # remove a leftover instance mount
-rm -rf huawei-benchmarks-metal
+rm -rf memory-pressure-benchmarks-metal
 
 # format the mount
 sudo mkfs -t xfs /dev/nvme2n1
 
-mkdir huawei-benchmarks-metal
+mkdir memory-pressure-benchmarks-metal
 # mount the nvme volumn
-sudo mount /dev/nvme2n1 huawei-benchmarks-metal
+sudo mount /dev/nvme2n1 memory-pressure-benchmarks-metal
 # change ownsership of the volume
-sudo chown -R ubuntu huawei-benchmarks-metal/
+sudo chown -R ubuntu memory-pressure-benchmarks-metal/
 
-git clone https://github.com/Tmonster/huawei-benchmarks.git huawei-benchmarks-metal
+git clone https://github.com/Tmonster/memory-pressure-benchmarks.git memory-pressure-benchmarks-metal
 
 # if you have an EBS volume, you can generate the data once, save it on the ebs volume, and transfer it
 # each time.
@@ -20,20 +20,20 @@ git clone https://github.com/Tmonster/huawei-benchmarks.git huawei-benchmarks-me
 if [[ $# -gt 0 ]]
 then
 	echo "Creating data"
-	cd huawei-benchmarks
+	cd memory-pressure-benchmarks
 	duckdb tpch-sf100.duckdb -c "install httpfs; load httpfs; call dbgen(sf=100);"
 	duckdb tpch-sf100.duckdb -c "export database 'tpch_data' (FORMAT CSV, HEADER 1);"
 	python3 hyper/load.py
-	cd ../huawei-benchmarks-metal
-	cp ../huawei-benchmarks/tpch-sf100.duckdb .
-	cp ../huawei-benchmarks/tpch-sf100.hyper .
-elif [[ ! -d "huawei-benchmarks-metal" ]]
+	cd ../memory-pressure-benchmarks-metal
+	cp ../memory-pressure-benchmarks/tpch-sf100.duckdb .
+	cp ../memory-pressure-benchmarks/tpch-sf100.hyper .
+elif [[ ! -d "memory-pressure-benchmarks-metal" ]]
 then
 	echo "no arguments passed. Copying data..."
-	echo "ERROR: directory ./huawei-benchmarks-metal does not exist"
+	echo "ERROR: directory ./memory-pressure-benchmarks-metal does not exist"
 else
-	cd huawei-benchmarks-metal/
-	cp ../huawei-benchmarks/tpch-sf100.duckdb .
-	cp ../huawei-benchmarks/tpch-sf100.hyper .
+	cd memory-pressure-benchmarks-metal/
+	cp ../memory-pressure-benchmarks/tpch-sf100.duckdb .
+	cp ../memory-pressure-benchmarks/tpch-sf100.hyper .
 	cd ..
 fi
