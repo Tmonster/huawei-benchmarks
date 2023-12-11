c14cc91d5df32e02d32b6e275df5620867c5ef64
Tom Ebergen <tom@ebergen.com>
2023-12-11 13:52:18 +0100

    remove eecute from insert statement

--- utils/poll_memory.py
+++ utils/poll_memory.py
@@ -72,7 +72,7 @@ def poll_meminfo_duckdb(data_db, lock_file, benchmark_name, benchmark, system, r
 
         now = time.time()
         log = benchmark_identifiers + "," + str(now) + "," + get_csv_line(parsed_mem_info) + "\n"
-        con.sql(f"INSERT INTO time_info VALUES ({log})").execute()
+        con.sql(f"INSERT INTO time_info VALUES ({log})")
 
         # Wait for 0.2 seconds before polling again
         time.sleep(0.2)
