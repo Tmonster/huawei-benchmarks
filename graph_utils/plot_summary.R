24c96f4a0da889e3a8dd58125b26ca51b74eca54
Tom Ebergen <tom@ebergen.com>
2024-01-09 16:17:32 +0100

    add an x if there is only 1 measurement

--- graph_utils/plot_summary.R
+++ graph_utils/plot_summary.R
@@ -25,12 +25,21 @@ for (benchmark_type in c('tpch', 'operators')) {
 
 
   dbExecute(con, "
-    Create or replace table results_x_y as select (MemTotal - MemAvailable)/1000000 as MemUsed, Time - StartTimes.start_time as time, results.query_name as query, results.system from results, StartTimes where StartTimes.system = results.system and  StartTimes.query = results.query_name and  StartTimes.run_type = results.run_type and StartTimes.benchmark = results.benchmark and StartTimes.benchmark_name = results.benchmark_name;")
+    Create or replace table results_x_y as select (MemTotal - MemAvailable)/1000000 as MemUsed, Time - StartTimes.start_time as time, results.query_name as query, results.system as system from results, StartTimes where StartTimes.system = results.system and  StartTimes.query = results.query_name and  StartTimes.run_type = results.run_type and StartTimes.benchmark = results.benchmark and StartTimes.benchmark_name = results.benchmark_name;")
+
+  dbExecute(con, "
+    create or replace table single_points as select query, max(time) as time, max(MemUsed) mem_used, count(*) as num_vals, system from results_x_y group by all having num_vals <= 1;")
+
+  single_points_duckdb <- dbGetQuery(con, "FROM single_points where system = 'duckdb'")
+  single_points_hyper <- dbGetQuery(con, "FROM single_points where system = 'hyper'")
 
   results <- dbGetQuery(con, "FROM results_x_y")
 
+
   ggplot(results, aes(x=time, y=MemUsed, col=system)) +
     geom_line() +
+    geom_point(data=single_points_duckdb, aes(x=time, y=mem_used, col=system), color="red", shape=4, size=2) +
+    geom_point(data=single_points_hyper, aes(x=time, y=mem_used, col=system), color="#00B8E7", shape=4, size=2) +
     facet_wrap(~query, ncol=5, scales="free_x") +
     ylim(0,NA) + 
     xlab("time [s]") +
