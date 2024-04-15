668b32a4f5562ff7437e875d2e74c4469eaf2e19
Tmonster <tom@ebergen.com>
2024-04-15 23:40:35 +0200

    more simple queries

--- benchmark-queries/tmm-queries/large_result_two_build_tables.sql
+++
@@ -1,6 +0,0 @@
--- create table big_table as select range a, range b, range c from range(10000);
-
-select * from big_table ta, big_table tb, big_table tc
-where 
-	ta.a = tb.a and
-	tb.a = tc.a;
---
+++ benchmark-queries/tmm-queries/two_build_tables.sql
@@ -0,0 +1,29 @@
+-- Table with 100M unique values for probing
+-- CREATE TABLE probe AS
+-- SELECT range i,
+--        range j
+-- FROM range(100000000);
+
+-- -- Table with 90M unique values for building
+-- CREATE TABLE build AS
+-- SELECT i, j
+-- FROM probe
+-- WHERE i % 10 != 5;
+
+-- -- Duplicate probe table to 400M
+-- INSERT INTO probe
+-- SELECT probe.*
+-- FROM probe, range(3);
+
+
+-- Building a JoinHashTable on the build table uses 90e6*(1+8+8+8+16)/1e9 = 3.7 GB
+-- We set our memory limit to 5 GB so that only one hash table would fit
+
+-- Now we join probe with 3x build
+SELECT max(i),
+       max(p.j),
+       max(b1.j),
+       max(b2.j)
+FROM probe AS p
+JOIN build AS b1 USING (i)
+JOIN build AS b2 USING (i);
