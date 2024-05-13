624ed499fa0d44adfeb5330e763c951aac2c8cde
Tom Ebergen <tom@ebergen.com>
2024-05-13 10:00:36 +0200

    commit working stuff

--- graph_utils/memory_limit_vs_query.R
+++ graph_utils/memory_limit_vs_query.R
@@ -10,28 +10,31 @@ library(duckdb)
 con <- dbConnect(duckdb(''))
 
 
-dbExecute(con, sprintf("CREATE TABLE filtered_data AS \
-                       SELECT execution_time, memory_limit, num_connections::VARCHAR as connections , \
-                        query \
-                      from 'tmm_GOOD_data.parquet'"));
+for (query_name in c('01_build_table', '02_build_tables', '03_build_tables')) {
 
-aggregated <- dbGetQuery(con, "FROM filtered_data")
+  dbExecute(con, sprintf("CREATE OR REPLACE TABLE filtered_data AS \
+                         SELECT execution_time, memory_limit, num_connections::VARCHAR as connections , \
+                          query \
+                        from 'tmm_GOOD_data.parquet' where query = '%s'", query_name));
 
-# ggplot(aggregated, aes(x=memory_limit, y=time)) +
-#   geom_line() +
-#   facet_wrap(~query, ncol=5, scales="free_y") +
-#   xlab("Memory limit [GB]") +
-#   ylab("Execution time [s]") +
-#   ylim(0, 110) +
-#   theme_bw()
+  aggregated <- dbGetQuery(con, "FROM filtered_data")
 
-ggplot(aggregated, aes(x=memory_limit, y=execution_time, color=connections)) +
-  geom_point(size=1) +
-  facet_wrap(~query, ncol=4, scales="free_y") +
-  xlab("Memory limit [GB]") +
-  ylab("Execution time [s]") +
-  ylim(0, 400) +
-  theme_bw()
+  # ggplot(aggregated, aes(x=memory_limit, y=time)) +
+  #   geom_line() +
+  #   facet_wrap(~query, ncol=5, scales="free_y") +
+  #   xlab("Memory limit [GB]") +
+  #   ylab("Execution time [s]") +
+  #   ylim(0, 110) +
+  #   theme_bw()
+
+  ggplot(aggregated, aes(x=memory_limit, y=execution_time, color=connections)) +
+    geom_point(size=1) +
+    facet_wrap(~query, ncol=4, scales="free_y") +
+    xlab("Memory limit [GB]") +
+    ylab("Execution time [s]") +
+    ylim(0, NA) +
+    theme_bw()
 
 
-ggsave("tpc-sf100-mem-limit-GOOD-connections.pdf", width=10, height=6)
+  ggsave(sprintf("tpc-sf100-mem-limit-GOOD-connections-%s.pdf", query_name), width=10, height=6)
+}
